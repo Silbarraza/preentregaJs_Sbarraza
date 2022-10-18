6c6d44b856f9d28f9f7faf15c26c7ae4d1de9a6d
Silvana Barraza <barrazasilvana@gmail.com>
2022-10-18 17:00:48 -0300

    Se agrego arrays

--- js/main.js
+++ js/main.js
@@ -1,88 +1,148 @@
 /* Gestor de turnos (gimnasio)*/
 
+//Funcion buscar cliente por dni
 
+function existeClienteConDNI (dni) {
 
-const clave = 333;
-let claveIngresada = parseInt(prompt ("Ingresa tu clave"));
-let cantidadDeIntentos = 0;
+    let encontrado = false;
 
-let claseIngresada ="";
-let numTurno = 0;
-
-//ingresar clave
-while ((claveIngresada !== clave) && (cantidadDeIntentos < 3)) {
-    alert ("La clave es incorrecta");
-         
-    claveIngresada = parseInt (prompt("Por favor ingresa tu clave nuevamente"));
-    
-    cantidadDeIntentos++;
-} 
-
-if (cantidadDeIntentos === 3) {
-    alert ("Supero la cantidad de intentos");
-}else {
-    //seleccion de la clase del gym   
-    claseIngresada = listaClases(prompt("Elegí la clase a la que asistirás"));
-    
-    while (claseIngresada == 0) {
-        alert ("La clase ingresada no corresponde a una clase activa de MS Gym");
-
-        claseIngresada = listaClases(prompt("Por favor, ingresa la clase a la que asistirás"));
+    for(const cliente of listaClientes) {
+        if(cliente.dni === dni) {
+        encontrado = true;
+        }
     }
-
-    //asignación de turno
-    cuentaTurnos ();
-
+    return encontrado;
 }
 
 
+function buscarClientePorDNI (dni) {
 
-function cuentaTurnos (){
- 
+    let clienteEncontrado;
+
+    for(let i = 0; i < listaClientes.length; i++) {
+        
+        if(listaClientes[i].dni === dni) {
+            clienteEncontrado = listaClientes[i].nombreYapellido;
+            break;
+        }
+
+    }
+
+    return clienteEncontrado;
+}
+
+
+//Clase para clientes
+class Cliente {
+    constructor (nombreYapellido, dni) {
+        this.nombreYapellido = nombreYapellido;
+        this.dni = dni;
+    }
+}
+
+//Array con lista de clientes
+const listaClientes = [];
+
+listaClientes.push(new Cliente("Silvana Barraza", "32043974"));
+listaClientes.push(new Cliente("Juan Gonzalez", "40223333"));
+listaClientes.push(new Cliente("Daniela Perez", "37854525"));
+
+//Clase con las clases del gym
+class ClaseDelGym {
+
+    constructor (nombreClase){
+        this.nombreClase = nombreClase;
+        this.estado = false;
+        this.cliente ="";
+    }
+
+    reservaClase (cliente) {
+        if(this.estado){
+            console.log("Esta clase ya esta reservada");
+        }else {
+            this.estado=true;
+            this.cliente=cliente;
+            console.log("La tu lugar fue reservado con éxito");
+        }
+
+    }
+
+    cancelaClase () {
+        if(!this.estado){
+            console.log("La clase esta disponible");
+        }else {
+            console.log("La reserva fue cancelada con éxito");
+        }
+
+    }
+}
+
+//Array con lista de las clases y horarios del gym
+const listaClasesGym = [];
+
+listaClasesGym.push(new ClaseDelGym("Zumba"));
+listaClasesGym.push(new ClaseDelGym("TRX"));
+listaClasesGym.push(new ClaseDelGym("Box Cardio"));
+listaClasesGym.push(new ClaseDelGym("Streching"));
+listaClasesGym.push(new ClaseDelGym("Entrenamiento Funcional"));
+listaClasesGym.push(new ClaseDelGym("Full Body"));
+listaClasesGym.push(new ClaseDelGym("Yoga"));
+
+//Ingreso DNI del cliente para login
+let dniIngresado = prompt("Ingrese su DNI");
+let cantidadDeIntentos = 0;
+
+while ((!existeClienteConDNI(dniIngresado)) && (cantidadDeIntentos < 3)){
+    dniIngresado = prompt("El DNI ingresado no es correcto, por favor ingreselo nuevamente");
+    cantidadDeIntentos++;
+}
+
+
+//Funcion para asignar el turno y reservar
+
+function asignacionTurno (){
     if (numTurno <= 3 ) {
         numTurno++;
-        alert(" Turno  N° "+numTurno+" reservado, "+ "te esperamos en la clase de " + claseIngresada + " ¡Muchas gracias!!!");
-
+        alert(" Turno  N° "+numTurno+" reservado, "+ "te esperamos en la clase de " + claseIngresada + " ¡Muchas gracias!!!");        
     } else {
         alert ("No hay turnos para la clase seleccionada");
     }
 }
 
+const clienteRegistrado = buscarClientePorDNI(dniIngresado);
+let claseIngresada= "";
+let numTurno = 0;
+let existeClase= "";
 
-function listaClases (clase){ 
+if (cantidadDeIntentos === 3) {
+    alert ("Supero la cantidad de intentos");
+}else {
+    //seleccion de la clase del gym 
+    claseIngresada = prompt("Hola " + clienteRegistrado + " elegi la clase a la que queres asisitir"); 
+    
+    //Buscamos usando some si la clase ingresada existe
+    existeClase = listaClasesGym.some( function (ClaseDelGym) {
+        return ClaseDelGym.nombreClase.toLowerCase() === claseIngresada.toLowerCase();
+    }); 
+    console.log(existeClase);
+    
+    //Si la clase se ingreso de forma incorrecta se pide que la ingrese de nuevo
+    while(!existeClase) {
+        alert ("La clase ingresada no corresponde a una clase activa de H&F Gym");
 
-    switch (clase){
+        existeClase = true;
 
-        case "SPINNING":
-        case "spinning":
-        case "Spinning":
-            return "Spinning";
-            break;
-        case "ENTRENAMIENTO FUNCIONAL":
-        case "entrenamiento funcional":
-        case "Entrenamiento Funcional":
-            return "Entrenamiento Funcional";
-            break;
-        case "trx":
-        case "TRX":
-            return "TRX";
-            break;
-        case "fullbody":
-        case "full body":
-        case "Full body":
-        case "FULL BODY":
-        case "FULLBODY":    
-        case "Full Body":    
-            return "Full Body";
-            break;
-        case "ZUMBA":
-        case "zumba":
-        case "Zumba":
-            return "Zumba";
-            break;
-        default:
-            return 0;
-            break;
+        claseIngresada = prompt("Por favor, ingresa la clase a la que asistirás");
+
+        existeClase = listaClasesGym.some( function (ClaseDelGym) {
+            return ClaseDelGym.nombreClase.toLowerCase() === claseIngresada.toLowerCase();
+        }); 
     }
-}
+    
+    //si se ingresa correctamente se asigna de turno y la clase queda reservada
+    asignacionTurno ();
 
+    //la clase pasa a estado reservado
+    claseIngresada.reservaClase(clienteRegistrado);
+
+}
